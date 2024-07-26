40a80a225c56ed58a5ce1aa125bd7224da8ea40a
jeongsuri <jeongsuri96@gmail.com>
2024-07-26 15:51:51 +0900

    Spring boot 초기 세팅 완료

--- src/main/java/org/oreochex/global/configs/SecurityConfig.java
+++ src/main/java/org/oreochex/global/configs/SecurityConfig.java
@@ -3,12 +3,20 @@ package org.oreochex.global.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //security 설정 해체
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { //인코더 방식 설정
+        return new BCryptPasswordEncoder(); //BCrypt형태로 Encoder수행
     }
 }
