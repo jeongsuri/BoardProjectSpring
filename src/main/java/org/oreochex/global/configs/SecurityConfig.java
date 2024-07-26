92f031c8aa8f0ce5917495fa875eb9313f926505
jeongsuri <jeongsuri96@gmail.com>
2024-07-26 14:55:06 +0900

    Spring boot SecurityConfig 설정

---
+++ src/main/java/org/oreochex/global/configs/SecurityConfig.java
@@ -0,0 +1,14 @@
+package org.oreochex.global.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //security 설정 해체
+        return http.build();
+    }
+}
