2faa46cd2b13fd86585107c3e30fe9fc81537278
jeongsuri <jeongsuri96@gmail.com>
2024-07-31 09:39:38 +0900

    UserDetailsService 구현

---
+++ src/main/java/org/oreochex/member/services/MemberInfoService.java
@@ -0,0 +1,47 @@
+package org.oreochex.member.services;
+
+import lombok.RequiredArgsConstructor;
+import org.oreochex.member.MemberInfo;
+import org.oreochex.member.constants.Authority;
+import org.oreochex.member.entities.Authorities;
+import org.oreochex.member.entities.Member;
+import org.oreochex.member.repositories.MemberRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+
+    private MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        //null값을 방지하기 위해 기본값을 USER로 설정.
+        List<Authorities> tmp = Objects.requireNonNullElse(member.getAuthorities(),
+                List.of(Authorities.builder()
+                .member(member)
+                .authority(Authority.USER)
+                        .build()));
+
+        List<SimpleGrantedAuthority> authorities = tmp.stream()
+                .map(a -> new SimpleGrantedAuthority(a.getAuthority().name()))
+                .toList();
+
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .member(member)
+                .authorities(authorities)
+                .build();
+    }
+}
