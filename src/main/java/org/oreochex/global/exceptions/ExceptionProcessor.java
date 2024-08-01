628d4fc4ddbbe99f4140e6e2906c6ca3bd10c8fa
jeongsuri <jeongsuri96@gmail.com>
2024-08-01 11:22:49 +0900

    자바스크립트 alert 에러처리

--- src/main/java/org/oreochex/global/exceptions/ExceptionProcessor.java
+++ src/main/java/org/oreochex/global/exceptions/ExceptionProcessor.java
@@ -1,6 +1,7 @@
 package org.oreochex.global.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.oreochex.global.exceptions.script.AlertException;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +11,18 @@ public interface ExceptionProcessor {
     @ExceptionHandler
     default ModelAndView errorHandler(Exception e, HttpServletRequest request){
 
+        ModelAndView mv = new ModelAndView();
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String tpl = "error/error";
+
         if(e instanceof CommonException commonException) {
             status = commonException.getStatus();
+
+            if(e instanceof AlertException){
+                tpl="common/_execute_script";
+                String script = String.format("alert('%s');", e.getMessage());
+            }
+
         }
 
         String url = request.getRequestURI();
@@ -20,13 +30,13 @@ public interface ExceptionProcessor {
 
         if(StringUtils.hasText(qs)) url += "?" + qs;
 
-        ModelAndView mv = new ModelAndView();
         mv.addObject("message", e.getMessage());
         mv.addObject("status", status.value());
         mv.addObject("method", request.getMethod());
         mv.addObject("path", url);
         mv.setStatus(status);
-        mv.setViewName("error/error");
+        mv.setViewName(tpl);
+
         return mv;
     }
 }
